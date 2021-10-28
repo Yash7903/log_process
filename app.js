93d50e80d9d1a3793533872bc3173037ee7b19df
Yash7903 <79798436+Yash7903@users.noreply.github.com>
2021-10-28 23:23:51 +0530

    Security level 2

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 //jshint esversion:6
-  require('dotenv').config();
   const express = require('express');
   const bodyParser = require('body-parser');
   const ejs = require('ejs');
   const mongoose = require('mongoose');
-  const encrypt = require('mongoose-encryption');
+  const md5 = require('md5');
 
 
   const app = express();
@@ -22,8 +21,6 @@
     password : String
   });
 
-
-  userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields:["password"] });
 
   const User = new mongoose.model('User', userSchema);
 
@@ -50,7 +47,7 @@
   app.post("/register", function(req, res){
     const newUser = new User({
       email: req.body.username,
-      password: req.body.password
+      password: md5(req.body.password)
     });
     newUser.save(function(err){
       if(err){
@@ -63,7 +60,7 @@
 
   app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email:username},function(err, foundUser){
       if(err){
